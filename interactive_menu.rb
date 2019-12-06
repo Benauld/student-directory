dfcab14d4b0ff2846d0f30358ce5fb94086209e8
benauld <ben@portobello-consultancy.co.uk>
2019-12-06 09:08:58 +0000

    Refactored interactive menu

---
+++ interactive_menu.rb
@@ -0,0 +1,71 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  @students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+  # add the student hash to the array
+  @students << {name: name, cohort: :november}
+  puts "Now we have #{@students.count} students"
+  # get another name from the user
+  name = gets.chomp
+  end
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  @students.each do |student|
+    puts " #{student[:name]} ( #{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def interactive_menu
+  loop do
+    # 1. print the menu and ask the user what to do
+    print_menu
+    # 2. read the input and put it in to a variable
+    # 3. do what the user has asked
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  # show some students_sub
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      # input some students
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't recognise that selection. Try again"
+  end
+end
+
+
+interactive_menu
